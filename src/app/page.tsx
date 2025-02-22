39dca640723a8654a71b8003186379fdbd3fa961
Артем Зотов <artem.zotov@webpp.ru>
2025-02-22 12:05:23 +0300

    Добавили анимацию

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Image
-        src="/next.svg"
-        alt="Profile"
-        width={1000}
-        height={1000}
-      />
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 5 }}
+      >
+        <Image
+          src="/next.svg"
+          alt="Profile"
+          width={1000}
+          height={1000}
+        />
+      </motion.div>
     </div>
   );
 }
