a2c4b21136243492f48aeba3cdef75c8d328c9cd
Артем Зотов <artem.zotov@webpp.ru>
2025-02-22 10:39:08 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.cntraveler.com",
+        port: "",
+        pathname: "/photos/**",
+        search: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
