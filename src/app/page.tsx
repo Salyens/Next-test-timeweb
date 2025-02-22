e4f06b0b456201db7d9e72ac185e4009f623407e
Артем Зотов <artem.zotov@webpp.ru>
2025-02-22 11:45:37 +0300

    Заменили картинку

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1>Hello World</h1>
       <Image
-        src="https://media.cntraveler.com/photos/5627b0d7c4dab9d441424e8b/16:9/w_1280,c_limit/oslo-norway-cr-getty.jpg"
+        src="/next.svg"
         alt="Profile"
         width={1000}
         height={1000}
