322145544eb6f0848c29c19f7855889ffdcb1df9
Артем Зотов <artem.zotov@webpp.ru>
2025-02-26 11:11:43 +0300

    Update About and Reviews sections with content and styling improvements

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <Container>
+    <Container className="flex flex-col gap-4 mb-4">
       <Title title="О нас" />
       <div className="relative w-full h-[50vh]">
         <Image
@@ -14,6 +14,20 @@ const About = () => {
           layout="fill"
           className="object-cover"
         />
+      </div>
+      <div className="flex flex-col gap-4 font-bold">
+        <p>
+          Гостевой Дом Кредо расположен во второй курортной
+          зоне, на тихой улице. До Колоннады, Нарзанной
+          галереи, входа в Национальный парк 700 метров.
+        </p>
+        <p>
+          Для вашего удобства и комфортного отдыха все
+          номера оснащены кухней и санузлом. Рядом магазины,
+          Кисловодская ярмарка, Свято-Никольский собор,
+          Мечеть. Во дворе садовая мебель, мангал. Парковка
+          уличная, рядом с домом.
+        </p>
       </div>
     </Container>
   );
