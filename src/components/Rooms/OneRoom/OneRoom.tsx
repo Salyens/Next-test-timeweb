a062784c4adb76ff39839e3d03bf0afa8d1b1f23
Артем Зотов <artem.zotov@webpp.ru>
2025-03-03 15:59:41 +0300

    Enable autoplay for reviews and add view-based animation to room cards
    - Uncomment autoplay for ReviewCarousel
    - Add Framer Motion view-tracking animation to OneRoom component
    - Use useInView hook to trigger fade-in effect for room cards

--- src/components/Rooms/OneRoom/OneRoom.tsx
+++ src/components/Rooms/OneRoom/OneRoom.tsx
@@ -1,31 +1,49 @@
 import Button from "@/components/Button";
+import { MotionDiv } from "@/components/MotionDiv";
 import { Room } from "@/types";
+import { useInView } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 
 const OneRoom = ({ room }: { room: Room }) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(ref, {
+    once: true,
+    margin: "-100px",
+  });
+
   return (
-    <Link
-      id={room.id.toString()}
-      href={`/room/${room.id}`}
-      className="group flex flex-col gap-2 font-bold  p-4 border cursor-pointer"
-    >
-      <div className="relative w-full h-64">
-        <Image
-          src={room.mainImage}
-          alt="room"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover group-hover:scale-105 transition-transform duration-300"
-        />
-      </div>
-      <p className="text-sm md:min-h-[50px]">
-        {room.title}
-      </p>
-      <p className="text-xl">{room.price} руб.</p>
-      <Button>Подробнее</Button>
-    </Link>
+    <>
+      <div ref={ref}></div>
+
+      <MotionDiv
+        initial={{ opacity: 0 }}
+        animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Link
+          id={room.id.toString()}
+          href={`/room/${room.id}`}
+          className="group flex flex-col gap-2 font-bold  p-4 border cursor-pointer"
+        >
+          <div className="relative w-full h-64">
+            <Image
+              src={room.mainImage}
+              alt="room"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-cover group-hover:scale-105 transition-transform duration-300"
+            />
+          </div>
+          <p className="text-sm md:min-h-[50px]">
+            {room.title}
+          </p>
+          <p className="text-xl">{room.price} руб.</p>
+          <Button>Подробнее</Button>
+        </Link>
+      </MotionDiv>
+    </>
   );
 };
 
